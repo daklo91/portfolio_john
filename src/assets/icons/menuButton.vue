c68320bc5aaf86d117ccead3bd8bd65c0876f7a9
daklo91 <coffeedog91@gmail.com>
2021-07-07 13:35:05 +0200

    Add MenuButton SVG

---
+++ src/assets/icons/menuButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg
+    width="30"
+    height="20"
+    viewBox="0 0 30 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path class="line-1" d="M0 1H30" stroke-width="1.6" />
+    <path class="line-2" d="M0 10H30" stroke-width="1.6" />
+    <path class="line-3" d="M0 19H30" stroke-width="1.6" />
+  </svg>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.line-1,
+.line-2,
+.line-3 {
+  stroke: var(--dark);
+}
+</style>
