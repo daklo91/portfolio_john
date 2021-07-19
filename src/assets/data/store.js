39c313e93e79c47b5f1e12bb70fbb923854f3dbb
daklo91 <coffeedog91@gmail.com>
2021-07-19 14:10:27 +0200

    Add store data (no vuex)

---
+++ src/assets/data/store.js
@@ -0,0 +1,18 @@
+export default {
+  blogPosts: [
+    {
+      title: "Making a design system from scratch",
+      date: "12 Feb 2020",
+      type: ["design", "pattern"],
+      description:
+        "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+    },
+    {
+      title: "Creating pixel perfect icons in Figma",
+      date: "20 May 2020",
+      type: ["figma", "icon design"],
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec commodo elit. Integer ac metus sed felis iaculis tempor quis a erat. Nulla fringilla non ligula sollicitudin porta.",
+    },
+  ],
+};
