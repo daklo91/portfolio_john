dc52949dc6b5eb26a91fec26c8e5031407a65dff
daklo91 <coffeedog91@gmail.com>
2021-07-23 14:11:12 +0200

    Add work page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Work from "../views/Work.vue";
 
 const routes = [
   {
@@ -10,11 +12,12 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: About,
+  },
+  {
+    path: "/work",
+    name: "Work",
+    component: Work,
   },
 ];
 
