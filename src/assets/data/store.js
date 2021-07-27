a5185b05f296a8cd209586f5f51eb27b74943c13
daklo91 <coffeedog91@gmail.com>
2021-07-27 22:15:05 +0200

    Add more blog posts

--- src/assets/data/store.js
+++ src/assets/data/store.js
@@ -2,17 +2,45 @@ export default {
   blogPosts: [
     {
       title: "Making a design system from scratch",
-      date: "12 Feb 2020",
+      date: "12 May 2020",
       type: ["design", "pattern"],
       description:
         "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
     },
     {
       title: "Creating pixel perfect icons in Figma",
-      date: "20 May 2020",
+      date: "12 Feb 2020",
       type: ["figma", "icon design"],
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec commodo elit. Integer ac metus sed felis iaculis tempor quis a erat. Nulla fringilla non ligula sollicitudin porta.",
+    },
+    {
+      title: "UI Interactions of the week",
+      date: "12 Jan 2019",
+      type: ["express", "handlebars"],
+      description:
+        "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+    },
+    {
+      title: "Making a design system from scratch",
+      date: "12 May 2020",
+      type: ["design", "pattern"],
+      description:
+        "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+    },
+    {
+      title: "Creating pixel perfect icons in Figma",
+      date: "12 Feb 2020",
+      type: ["figma", "icon design"],
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec commodo elit. Integer ac metus sed felis iaculis tempor quis a erat. Nulla fringilla non ligula sollicitudin porta.",
+    },
+    {
+      title: "UI Interactions of the week",
+      date: "12 Jan 2019",
+      type: ["express", "handlebars"],
+      description:
+        "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
     },
   ],
   workPosts: [
