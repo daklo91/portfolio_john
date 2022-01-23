60c4ab608d745c3c32d5e681d79d7122533fce38
daklo91 <coffeedog91@gmail.com>
2022-01-23 13:23:58 +0100

    Remove about page, remove timer on page change

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
-import About from "../views/About.vue";
 import Work from "../views/Work.vue";
 import Blog from "../views/Blog.vue";
 import BlogPost from "../views/BlogPost.vue";
@@ -11,11 +10,6 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: About,
   },
   {
     path: "/work",
@@ -41,11 +35,7 @@ const routes = [
 
 const router = createRouter({
   scrollBehavior() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve({ left: 0, top: 0 });
-      }, 150);
-    });
+    return { top: 0 };
   },
   history: createWebHashHistory(),
   routes,
